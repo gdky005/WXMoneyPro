f885b6483140730f60327f6f2bdb3872df6a10d7
qingw <qingw@jumei.com>
2018-10-20 11:59:32 +0800

    1. 修改零钱样式。

--- app/src/main/java/com/gdky005/wxmoneydemo/LQActivity.java
+++ app/src/main/java/com/gdky005/wxmoneydemo/LQActivity.java
@@ -1,5 +1,9 @@
 package com.gdky005.wxmoneydemo;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.TextView;
+
 import com.blankj.utilcode.util.BarUtils;
 
 import team.zhuoke.sdk.base.BaseActivity;
@@ -14,6 +18,15 @@ public class LQActivity extends BaseActivity {
     protected void initViews() {
         BarUtils.setStatusBarAlpha(this, 0);
         BarUtils.setStatusBarLightMode(this, true);
+
+        TextView textView = findViewById(R.id.right_btn);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, MXActivity.class);
+                mContext.startActivity(intent);
+            }
+        });
     }
 
     @Override
